3b44a22615517ccc51e046b2bd89c1d7bee95d6e
Sateesh Janapana <sateeshjanapana3@gmail.com>
2024-05-09 20:21:41 +0530

    My first code commiting into git hub

---
+++ src/main/java/Automation/SampleGitBuhProject/App.java
@@ -0,0 +1,13 @@
+package Automation.SampleGitBuhProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/orangehrm/testcases/SampleCls.java
@@ -0,0 +1,11 @@
+package com.orangehrm.testcases;
+
+public class SampleCls {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hi This is first project check in to git hub");
+	}
+
+}
